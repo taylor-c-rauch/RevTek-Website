7b30f654d76c7eba382a08445e6413a31bd1d5c0
Christian Lessard <christian@d-172-27-79-112.dhcp.virginia.edu>
2018-06-13 15:27:49 -0400

    first commit

---
+++ revtek/src/Authentication.js
@@ -0,0 +1,60 @@
+import React, {Component} from 'react';
+import { Input, Button } from 'antd';
+import fire from './fire';
+
+export default class Authentication extends Component {
+  constructor(){
+    super();
+    this.state = {
+      email: "",
+      username: "",
+      password: "",
+      status: ""
+    }
+  }
+
+  signup = e => {
+    e.preventDefault();
+    fire
+      .auth()
+      .createUserWithEmailAndPassword(this.state.email, this.state.password)
+      .then(u => {
+        var user = fire.auth().currentUser;
+        this.logUser(user);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  };
+
+  logUser = user => {
+    let ref = fire.database().ref("users");
+    let obj = {
+      email: this.state.email,
+      username: this.state.username,
+      password: this.state.password,
+      status: this.state.status
+
+    };
+    ref.push(obj);
+  };
+
+  handleUserInput = e => {
+    this.setState({
+      [e.target.id]: e.target.value
+    });
+
+  };
+
+  render() {
+    return (
+      <div>
+        <Input id="email" placeholder="Email" onChange={e => this.handleUserInput(e)} />
+        <Input id="username" placeholder="Username" onChange={e => this.handleUserInput(e)} />
+        <Input id="password" placeholder="Password" onChange={e => this.handleUserInput(e)} />
+        <Input id="status" placeholder="Status" onChange={e => this.handleUserInput(e)} />
+        <Button type="primary" onClick={e => this.signup(e)}>Submit</Button>
+      </div>
+    )
+  }
+}
