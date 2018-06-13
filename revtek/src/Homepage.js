dc96d3f1c150c4f4cd7aa9733dda802896c42212
Edward Radion <er2cg@virginia.edu>
2018-06-13 15:07:43 -0400

    Homepage wireframe"om

---
+++ revtek/src/Homepage.js
@@ -0,0 +1,52 @@
+import "./App.js";
+import Background from "./homepage-pic.jpg";
+import "./Homepage.css";
+import React, { Component } from "react";
+
+import { Card, Row, Col, Button, Meta } from "antd";
+
+export default class Homepage extends Component {
+  render() {
+    return (
+      <div>
+        <section
+          style={{
+            backgroundImage: `url(${Background})`,
+            height: 470,
+            width: "100%",
+            backgroundSize: "cover",
+            overflow: "hidden"
+          }}
+        >
+          >
+          <Card
+            style={{ width: "70%" }}
+            title="RevTek matches top talent with meaningful projects"
+          >
+            <p>
+              {" "}
+              Our highly trained interns and alumni will work with you to build
+              products to suit your ever-changing needs
+            </p>
+          </Card>
+        </section>
+
+        <Row type="flex" justify="center">
+          <p> Who are we? </p>
+        </Row>
+        <Row type="flex" justify="center">
+          <p>
+            {" "}
+            At RevTek, we want to empower our employees to make meaningful
+            change in the world around them through the implementation of
+            world-class software.
+          </p>
+        </Row>
+        <Row type="flex" justify="center">
+          We have built a community of developers with the expertise to
+          flawlessly carry out your projects and ideas.
+        </Row>
+      </div>
+    );
+  }
+}
