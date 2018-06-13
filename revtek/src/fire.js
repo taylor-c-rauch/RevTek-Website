a1b4d4bf4c4bd3b705125e285fefe713157993bc
Taylor Rauch <Taylor@d-172-25-79-68.dhcp.virginia.edu>
2018-06-13 10:09:17 -0400

    add firebase file

---
+++ revtek/src/fire.js
@@ -0,0 +1,13 @@
+import firebase from "firebase";
+
+var config = {
+  apiKey: "AIzaSyD10gz2xwO7hoFc9dRCKel4GmJCxyeLU-U",
+  authDomain: "revtek-website.firebaseapp.com",
+  databaseURL: "https://revtek-website.firebaseio.com",
+  projectId: "revtek-website",
+  storageBucket: "revtek-website.appspot.com",
+  messagingSenderId: "818197254936"
+};
+firebase.initializeApp(config);
+
+export default fire;
