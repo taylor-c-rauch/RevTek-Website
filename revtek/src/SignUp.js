83969c31ec6870cd56f3d1ce84718a7b74c321a4
Manana Metreveli <mananametreveli@d-172-25-79-133.dhcp.virginia.edu>
2018-06-13 17:27:03 -0400

    Adds a confirmation message after signing up

--- revtek/src/SignUp.js
+++ revtek/src/SignUp.js
@@ -12,7 +12,8 @@ export default class SignUp extends Component {
       fullname: "",
       password: "",
       status: "",
-      user: null
+      user: null, 
+      clicked: false
     }
   }
 
@@ -28,8 +29,10 @@ export default class SignUp extends Component {
       .catch(error => {
         console.log(error);
       });
+    this.setState({clicked: true})
   };
 
+  // When the submit button is clicked, the user input gets put on firebase
   logUser = user => {
     const username = this.state.username;
     const usernameRef = fire.database().ref("users/" + username).set({
@@ -41,6 +44,7 @@ export default class SignUp extends Component {
     })
   };
 
+  // updates each input's corresponding state
   handleUserInput = e => {
     this.setState({
       [e.target.id]: e.target.value
@@ -49,36 +53,46 @@ export default class SignUp extends Component {
   };
 
   render() {
-    return (
-      <section style={{
-        backgroundImage: `url(${Background})`,
-        height: 800,
-        width: '100%',
-        backgroundSize: 'cover',
-        overflow: 'hidden'
-      }}>
-        <div>
-          <h1>Sign Up</h1>
-          <Row>
-            <Col span={24}>
-              <Input style={{width: '50%'}} id="email" placeholder="Email" onChange={e => this.handleUserInput(e)} />
-            </Col>
-            <Col span={24}>
-              <Input style={{width: '50%'}} id="username" placeholder="Username" onChange={e => this.handleUserInput(e)} />
-            </Col>
-            <Col span={24}>
-              <Input style={{width: '50%'}} id="fullname" placeholder="Fullname" onChange={e => this.handleUserInput(e)} />
-            </Col>
-            <Col span={24}>
-              <Input style={{width: '50%'}} id="password" placeholder="Password" onChange={e => this.handleUserInput(e)} />
-            </Col>
-            <Col span={24}>
-              <Input style={{width: '50%'}} id="status" placeholder="Status" onChange={e => this.handleUserInput(e)} />
-            </Col>
-            <Button type="primary" onClick={e => this.signup(e)}>Submit</Button>
-          </Row>
+    if (this.state.clicked === false) {
+      return (
+        <section style={{
+          backgroundImage: `url(${Background})`,
+          height: 800,
+          width: '100%',
+          backgroundSize: 'cover',
+          overflow: 'hidden'
+        }}>
+          <div>
+            <h1>Sign Up</h1>
+            <Row>
+              <Col span={24}>
+                <Input style={{width: '50%'}} id="email" placeholder="Email" onChange={e => this.handleUserInput(e)} />
+              </Col>
+              <Col span={24}>
+                <Input style={{width: '50%'}} id="username" placeholder="Username" onChange={e => this.handleUserInput(e)} />
+              </Col>
+              <Col span={24}>
+                <Input style={{width: '50%'}} id="fullname" placeholder="Fullname" onChange={e => this.handleUserInput(e)} />
+              </Col>
+              <Col span={24}>
+                <Input style={{width: '50%'}} id="password" placeholder="Password" onChange={e => this.handleUserInput(e)} />
+              </Col>
+              <Col span={24}>
+                <Input style={{width: '50%'}} id="status" placeholder="Status" onChange={e => this.handleUserInput(e)} />
+              </Col>
+              <Button type="primary" onClick={e => this.signup(e)}>Submit</Button>
+            </Row>
+          </div>
+        </section>
+      )
+    }
+    else {
+      return (
+        <div style={{ background: '#ECECEC', padding: '30px' }}>
+          <h1>Thank you for signing up!</h1>
+          <h3> Registration requires instructor approval. You will receive an email soon regarding your account status</h3>
         </div>
-      </section>
-    )
+      )
+    }
   }
 }
