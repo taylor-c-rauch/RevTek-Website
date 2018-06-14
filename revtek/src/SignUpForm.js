4077725a22f5bbdb7471ebfc5f419d14b0cde97f
Taylor Rauch <Taylor@d-172-25-79-16.dhcp.virginia.edu>
2018-06-14 14:21:50 -0400

    fix login

--- revtek/src/SignUpForm.js
+++ revtek/src/SignUpForm.js
@@ -1,7 +1,8 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import Background from './assets/homePhoto.jpg';
 import { Input, Button, Row, Col } from 'antd';
 import fire from './fire';
+import TopBar from "./top-bar";
 
 export default class SignUpForm extends Component {
   render() {
@@ -13,23 +14,24 @@ export default class SignUpForm extends Component {
         backgroundSize: 'cover',
         overflow: 'hidden'
       }}>
+        <TopBar status="home" />
         <div>
           <h1>Sign Up</h1>
           <Row>
             <Col span={24}>
-              <Input style={{width: '50%'}} id="email" placeholder="Email" onChange={e => this.handleUserInput(e)} />
+              <Input style={{ width: '50%' }} id="email" placeholder="Email" onChange={e => this.handleUserInput(e)} />
             </Col>
             <Col span={24}>
-              <Input style={{width: '50%'}} id="username" placeholder="Username" onChange={e => this.handleUserInput(e)} />
+              <Input style={{ width: '50%' }} id="username" placeholder="Username" onChange={e => this.handleUserInput(e)} />
             </Col>
             <Col span={24}>
-              <Input style={{width: '50%'}} id="fullname" placeholder="Fullname" onChange={e => this.handleUserInput(e)} />
+              <Input style={{ width: '50%' }} id="fullname" placeholder="Fullname" onChange={e => this.handleUserInput(e)} />
             </Col>
             <Col span={24}>
-              <Input style={{width: '50%'}} id="password" placeholder="Password" onChange={e => this.handleUserInput(e)} />
+              <Input style={{ width: '50%' }} id="password" placeholder="Password" onChange={e => this.handleUserInput(e)} />
             </Col>
             <Col span={24}>
-              <Input style={{width: '50%'}} id="status" placeholder="Status" onChange={e => this.handleUserInput(e)} />
+              <Input style={{ width: '50%' }} id="status" placeholder="Status" onChange={e => this.handleUserInput(e)} />
             </Col>
             <Button type="primary" onClick={e => this.signup(e)}>Submit</Button>
           </Row>
