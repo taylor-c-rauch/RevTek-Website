2b99f36ce8094cdbf46027c43b4ac46e0fe4c495
Manana Metreveli <mananametreveli@d-172-25-79-117.dhcp.virginia.edu>
2018-06-15 14:57:09 -0400

    Pushes the task and hours to firebase under the logged in user's branch

--- revtek/src/profilepage.js
+++ revtek/src/profilepage.js
@@ -32,13 +32,6 @@ export default class Profile extends Component {
             task: '', 
             hours: '',
         })
-
-        // const todoRef = fire.database().ref('todo');
-        // const todo = {
-        //   task: this.state.task,
-        //   hours: this.state.hours, 
-        // }
-        // todoRef.push(todo);
     }
 
      // retrieves the information from firebase so it can be rendered on the screen
