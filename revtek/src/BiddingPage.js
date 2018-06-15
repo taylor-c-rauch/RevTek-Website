99ad9387f9dc8663a2e31af57b9744deece4d50d
Bailey Snead <Bailey@d-172-25-79-169.dhcp.virginia.edu>
2018-06-15 17:29:56 -0400

    fix bracket error

--- revtek/src/BiddingPage.js
+++ revtek/src/BiddingPage.js
@@ -66,8 +66,7 @@ export default class BiddingPage extends React.Component {
         <Button type="primary" disabled={!this.state.onDisabled}>Submit Bid</Button>
         </Card>
         )}
-        </div>
+        </div>)
     }
 
     }
-}
