75332ea27c60a50c6c3c332bbd2eb3c17ec080da
Manana Metreveli <mananametreveli@d-172-25-79-135.dhcp.virginia.edu>
2018-06-18 14:46:44 -0400

    Added an edit and remove user button

--- revtek/src/UserList.js
+++ revtek/src/UserList.js
@@ -1,14 +1,26 @@
 import React from 'react'; 
 import ReactDom from 'react-dom'; 
-import { Row, Col, Card } from 'antd';
+import { Row, Col, Card, Button } from 'antd';
 import fire from "./fire.js";
 
 export default class UserList extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            allUsers: []
+            allUsers: [], 
+            clicked: false, 
         }
+    }
+
+    handleClick = e => {
+        this.setState({
+            clicked: !this.state.clicked,
+        })
+    }
+
+    removeItem(itemId) {
+        const itemRef = fire.database().ref('users/' + this.props.userID);
+        itemRef.remove();
     }
 
     componentDidMount(){
@@ -27,24 +39,51 @@ export default class UserList extends React.Component {
 
 
     render() {
-        return (
-           <div>
-                <Row gutter={16}>
-                <Col span={8}>
-                <h1> User Profiles </h1> 
-                    {this.state.allUsers.map((user) => {
-                        return (
-                            <div style={{ background: '#ECECEC', padding: '30px' }}>
-                            <Card title={user.fullname} bordered={false} style={{ width: 200 }}>
-                                <p>{user.email}</p>
-                                <p>{user.username}</p>
-                            </Card>
-                            </div> 
-                        )
-                    })}
-                </Col> 
-                </Row> 
-            </div> 
-        ); 
+        if (this.state.clicked == true) {
+            return (
+               <div>
+                     <Row gutter={16}>
+                     <Col span={8}>
+                     <h1> User Profiles </h1> 
+                         <Button size="medium" onClick={this.handleClick}>Done</Button>
+                         {this.state.allUsers.map((user) => {
+                             return (
+                                 <div style={{ background: '#ECECEC', padding: '30px' }}>
+                                 <Card title={user.fullname} bordered={false} style={{ width: 200 }}>
+                                     <p>Username: {user.username}</p>
+                                     <p>Email: {user.email}</p>
+                                     <Button size="medium" onClick={() => this.removeItem(user.id)}>Remove User</Button>
+                                 </Card>
+                                 </div> 
+                             )
+                         })}
+                     </Col> 
+                     </Row> 
+                 </div> 
+            )
+        }
+        else {
+            return (
+                <div>
+                     <Row gutter={16}>
+                     <Col span={8}>
+                     <h1> User Profiles </h1> 
+                         <Button size="medium" onClick={this.handleClick}>Edit</Button>
+                         {this.state.allUsers.map((user) => {
+                             return (
+                                 <div style={{ background: '#ECECEC', padding: '30px' }}>
+                                 <Card title={user.fullname} bordered={false} style={{ marginTop: 8 }}>
+                                     <p>Username: {user.username}</p>
+                                     <p>Email: {user.email}</p>
+                                 </Card>
+                                 </div> 
+                             )
+                         })}
+                     </Col> 
+                     </Row> 
+                 </div> 
+             ); 
+
+        }
     }
 }
