6dbc039d5d5160c828529cbbaf552d548ccbdabc
Bailey Snead <Bailey@d-172-25-79-191.dhcp.virginia.edu>
2018-06-20 11:09:24 -0400

    format user list

--- revtek/src/UserList.js
+++ revtek/src/UserList.js
@@ -1,7 +1,8 @@
 import React from 'react'; 
 import ReactDom from 'react-dom'; 
-import { Row, Col, Card, Button, Select} from 'antd';
+import { Row, Col, Card, Button, Select, Tag} from 'antd';
 import fire from "./fire.js";
+import "./UserList.css";
 
 
 const Option = Select.Option
@@ -63,10 +64,10 @@ export default class UserList extends React.Component {
     render() {
         const isClicked = this.state.clicked; 
         return (
-            <div>
+            <div className="userProfs">
                  <Row gutter={16}>
                  <Col span={8}>
-                 <h1> User Profiles </h1> 
+                 <h1 className="userHeader"> User Profiles </h1> 
                      {isClicked ? <Button size="medium" onClick={this.handleClick}>Done</Button> : <Button size="medium" onClick={this.handleClick}>Edit</Button>} 
                      <div style={{ background: '#ECECEC', padding: '30px' }}>
                         <div>
@@ -75,7 +76,11 @@ export default class UserList extends React.Component {
                             if (user.approved == false) {
                                 return (
                                 <div>
-                                <Card title={user.fullname} bordered={false} style={{ marginTop: 8 }}>
+                                <Card className="userCards" 
+                                      title={<h1 className="cardTitle">{user.fullname} </h1>} 
+                                      bordered={false} 
+                                      style={{ marginTop: 8 }}
+                                      extra={<img width="100px" height="100px" alt="example" src={user.profilepic}/>} >
                                     <p>Username: {user.username}</p>
                                     <p>Email: {user.email}</p>
                                     {isClicked ?  <div> <Button size="medium" onClick={() => this.removeItem(user.id)}>Remove User</Button> <Button size="medium" onClick={() => this.handleCreate(user.id)}> Approve New User </Button> 
@@ -95,10 +100,24 @@ export default class UserList extends React.Component {
                         <div>
                         <h2> Approved Users </h2> 
                         {this.state.allUsers.map((user) => {
+                            let skills = [];
+                            let curSkills = user.skills;
+                                if(curSkills) {
+                                    Object.keys(curSkills).forEach((key) => {
+                                        let skill = curSkills[key].skill;
+                                        skills.push(skill)
+                                        console.log(skill)
+                                        
+                                    });
+                                };
                             if (user.approved == true) {
                                 return (
-                                <div>
-                                <Card title={user.fullname} bordered={false} style={{ marginTop: 8 }}>
+                                <div style={{ background: '#ECECEC', padding: '30px'}}>
+                                <Card className="userCards"
+                                      title={<h1 className="cardTitle">{user.fullname} </h1>} 
+                                      bordered={false} 
+                                      style={{ marginTop: 8 }}
+                                      extra={<img width="100px" height="100px" alt="example" src={user.profilepic}/>} >
                                     <p>Username: {user.username}</p>
                                     <p>Email: {user.email}</p>
                                     {isClicked ?  <div> <Button size="medium" onClick={() => this.removeItem(user.id)}>Remove User</Button>
@@ -109,6 +128,12 @@ export default class UserList extends React.Component {
                                             <Option value="administrator">Administrator</Option>
                                         </Select> 
                                     </div>: null}
+                                    Skills: {skills.map(skill => {
+                                        let theSkill = skill;
+                                        return (
+                                        <Tag> {theSkill} </Tag>
+                                        )
+                                    })}
                                 </Card>
                                 </div>
                                 );
