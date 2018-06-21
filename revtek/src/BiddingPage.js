cceccadbaea00e77fb03972851bd0808e71bcb39
Ian J. McCray <ianmccray800@gmail.com>
2018-06-21 14:24:20 -0400

    bidding page styling

--- revtek/src/BiddingPage.js
+++ revtek/src/BiddingPage.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { Card, Layout, Input, Button } from 'antd';
 import fire from './fire.js'
+import "./BiddingPage.css"
 
 export default class BiddingPage extends React.Component {
   constructor(props) {
@@ -58,26 +59,30 @@ export default class BiddingPage extends React.Component {
 
     const { Header, Footer, Sider, Content } = Layout;
     return (
-      <div style={{ background: '#ECECEC' }}>
-        {this.state.data.map((x) => {
-          if (x.contractApproved === true) {
-            return (
-              <div>
-                <Card title={x.client} style={{ marginLeft: 30, marginRight: 30, marginTop: 20, marginBottom: 20 }}>
-                  <p><strong>Project Name: {x.project}</strong></p>
-                  <p>Description: {x.description}</p>
-                  <p>Email: {x.email}</p>
-                  <p>Number of Interns: {x.numinterns}</p>
-                  <p>Skills: {x.skills}</p>
-                  <Input placeholder="Pay Rate" id="payRate" onChange={e => this.handleUserInput(e)} />
-                  <Input placeholder="Estimated Hours" id="estHours" onChange={e => this.handleUserInput(e)} />
-                  <Button type="primary" >Submit Bid</Button>
-                </Card>
-              </div>
-            )
-          }
-        })}
-      </div>)
+      <div class="Overall" style={{ background: '#ECECEC' }}>
+        <h1 style={{ background: '#ECECEC' }} className="Header1"> Available Contracts </h1>
+        <div style={{ background: '#ECECEC', marginBottom: 30}}>
+          {this.state.data.map((x) => {
+            if (x.contractApproved === true) {
+              return (
+                <div style={{ background: '#ECECEC', marginBottom: 30 }}>
+                  <Card title={x.client} style={{ marginLeft: 30, marginRight: 30}}>
+                    <p><strong>Project Name: {x.project}</strong></p>
+                    <p>Description: {x.description}</p>
+                    <p>Email: {x.email}</p>
+                    <p>Number of Interns: {x.numinterns}</p>
+                    <p>Required Skills: {x.skills}</p>
+                    <Input placeholder="Pay Rate" id="payRate" onChange={e => this.handleUserInput(e)} />
+                    <Input placeholder="Estimated Hours" id="estHours" onChange={e => this.handleUserInput(e)} />
+                    <Button type="primary" >Submit Bid</Button>
+                  </Card>
+                </div>
+              )
+            }
+          })}
+        </div>
+      </div>
+      )
   }
 
 }
