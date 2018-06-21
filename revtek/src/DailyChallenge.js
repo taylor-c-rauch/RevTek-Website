61e8acd5fc9d2713a3b9dac569e202318de215f1
Christian Lessard <christian@d-172-27-79-159.dhcp.virginia.edu>
2018-06-21 16:51:42 -0400

    style changes made to daily challenge page

--- revtek/src/DailyChallenge.js
+++ revtek/src/DailyChallenge.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Layout, Form, Input, Icon, Button, Card, Row, Col } from 'antd';
+import { Layout, Form, Input, Icon, Button, Card, Row, Col, Alert } from 'antd';
 import './DailyChallenge.css'
 import TopBar from "./top-bar";
 import fire from './fire.js';
@@ -15,7 +15,8 @@ export default class DailyChallenge extends Component{
 			challengeName: "",
 			dueDate: "",
 			challengeDescription: "",
-			gitHubLink: "None"
+			gitHubLink: "None",
+			submitted: false
 		})
 	}
 
@@ -36,7 +37,7 @@ export default class DailyChallenge extends Component{
 	            		challengeName: challengesSnapshot[challenge].name,
 	            		dueDate: challengesSnapshot[challenge].duedate,
 	            		challengeDescription: challengesSnapshot[challenge].description
-	            	}) 
+	            	})
             }
 
             this.setState({
@@ -46,7 +47,7 @@ export default class DailyChallenge extends Component{
 	}
 
 	handleChange = (e) => {
-		e.preventDefault();	
+		e.preventDefault();
     	this.setState({
       		gitHubLink: e.target.value
     	});
@@ -56,46 +57,79 @@ export default class DailyChallenge extends Component{
 		e.preventDefault();
 
 		const challengesRef = fire.database().ref('challenges/' + this.state.challengeName + '/submissions');
-		challengesRef.child(this.props.person.fullname).set({ 
+		challengesRef.child(this.props.person.fullname).set({
             Name: this.props.person.fullname,
             gitHubLink: this.state.gitHubLink
         });
+		this.setState({
+			gitHubLink: '',
+			submitted: true
+		})
+	}
+
+	renderMessage = () => {
+		if (this.state.submitted === true) {
+			return (
+				<div style={{padding: 10}}>
+					<Alert message={`You have submitted successfully!`} type="success" showIcon />
+				</div>
+			)
+		} else if (this.state.submitted === false) {
+			return(
+				<div style={{padding: 10}}>
+					<Alert message={`You need to submit a link!`} type="info" showIcon />
+				</div>
+			)
+		}
 	}
 
 	render() {
 		console.log(this.state.challengeName)
 		return (
-			<Row>
-				<Col span = {24}>
-					<Content className="info">
-						<Card title={this.state.challengeName + " Due Date:" + this.state.dueDate} bordered={true}>
-							<p>{this.state.challengeDescription}</p>
-						</Card>
-					</Content>
-				</Col>
-				<Col span={12} offset = {6}>
-					<Content className = "submission">
-						<Card bordered={true}>
-							<p>Enter your Github Link</p>
-							<Form layout="inline" onSubmit={this.handleSubmit}>
-								<FormItem>
-						        	<Input 
-						        	prefix={<Icon type="link" 
-						        	style={{ color: 'rgba(0,0,0,.25)' }} />} 
-						        	placeholder="GitHub Link"
-						        	onChange={this.handleChange} 
-						        	/>
-						        </FormItem>
-								<FormItem>
-						        	<Button type="primary" htmlType="submit">
-						        	Submit
-						        	</Button>
-						        </FormItem>  
-							</Form>
-						</Card>
-					</Content>
-				</Col>	
-			</Row>
+			<div style={{backgroundColor: 'white', height: 800}}>
+				<div style={{textAlign: 'center', paddingTop: 30}}>
+					<h1 style={{fontSize: 35, fontFamily: 'Roboto'}} className="RobotoFont">Daily Challenge</h1>
+				</div>
+				<Row>
+					<Col span = {24}>
+
+							<Content className="info">
+								<div style={{backgroundColor: '#c4c4c4', padding: 20}}>
+								<Card title={this.state.challengeName + " Due Date:" + this.state.dueDate} bordered={true}>
+									<p>{this.state.challengeDescription}</p>
+								</Card>
+								</div>
+							</Content>
+
+					</Col>
+					<Col span={12} offset = {6}>
+						<Content className = "submission">
+							<div style={{backgroundColor: '#c4c4c4', padding: 20}}>
+								<Card bordered={true}>
+									<p>Enter your Github Link</p>
+									<Form layout="inline" onSubmit={this.handleSubmit}>
+										<FormItem>
+								        	<Input
+								        	prefix={<Icon type="link"
+								        	style={{ color: 'rgba(0,0,0,.25)' }} />}
+								        	placeholder="GitHub Link"
+													value={this.state.gitHubLink}
+								        	onChange={this.handleChange}
+								        	/>
+								        </FormItem>
+										<FormItem>
+								        	<Button type="primary" htmlType="submit">
+								        	Submit
+								        	</Button>
+								        </FormItem>
+												{this.renderMessage()}
+									</Form>
+								</Card>
+						</div>
+						</Content>
+					</Col>
+				</Row>
+		</div>
 			)
 	}
 }
