005e39227c969877593b46fd2b452a699acae20a
Taylor Rauch <Taylor@students-MacBook-Pro-4.local>
2018-06-21 20:57:11 -0400

    last minute fixes

--- revtek/src/BiddingPage.js
+++ revtek/src/BiddingPage.js
@@ -17,6 +17,7 @@ export default class BiddingPage extends React.Component {
   componentDidUpdate(prevProps) {
 
     if (this.props.userID !== prevProps.userID) {
+
       const contractsRef = fire.database().ref('contracts');
       let contractVals = [];
       let contractKeys = [];
@@ -82,30 +83,30 @@ export default class BiddingPage extends React.Component {
 
       <div class="Overall" style={{ background: '#ECECEC' }}>
         <h1 style={{ background: '#ECECEC' }} className="Header1"> Available Contracts </h1>
-        <div style={{ background: '#ECECEC'}}>
-        {this.state.data.map((x) => {
-          if (x.contractApproved === true) {
-            return (
-              <div>
-              <br/>
-              <div>
-              <div
-                    style={{
-                      background: "#c4c4c4",
-                      padding: "5px",
-                      padding: "5px",
-                      width: "90%",
-                      margin: "0 auto",
-                    }}
-                  >
-                <ContractTile style={{ marginLeft: 30, marginRight: 30}} userID={this.props.userID} person={this.props.person} email={x.email} numinterns={x.numinterns} client={x.client} project={x.project} skills={x.skills} description={x.description} />
+        <div style={{ background: '#ECECEC' }}>
+          {this.state.data.map((x) => {
+            if (x.contractApproved === true) {
+              return (
+                <div>
+                  <br />
+                  <div>
+                    <div
+                      style={{
+                        background: "#c4c4c4",
+                        padding: "5px",
+                        padding: "5px",
+                        width: "90%",
+                        margin: "0 auto",
+                      }}
+                    >
+                      <ContractTile style={{ marginLeft: 30, marginRight: 30 }} userID={this.props.userID} person={this.props.person} email={x.email} numinterns={x.numinterns} client={x.client} project={x.project} skills={x.skills} description={x.description} />
+                    </div>
+                  </div>
                 </div>
-              </div>
-              </div>
-  
-            )
-          }
-        })}
+
+              )
+            }
+          })}
         </div>
       </div>)
 
